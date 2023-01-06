daab9035897765b3c99884d283c38885d33d3264
mentalicurse <121573729+mentalicurse@users.noreply.github.com>
2023-01-06 21:58:29 +0700

    Update huffman_decoded.cpp

--- huffman_decoded.cpp
+++ huffman_decoded.cpp
@@ -45,7 +45,7 @@ Node *BuildTree (priority_queue < Node, vector < Node >, Node > htree)
 }
 
 void DecodingHuffman(){
-	int *ABC = new int[256];
+  long int *ABC = new long int[256];
   for (int i = 0; i < 256; i++)
     {
       ABC[i] = 0;
@@ -56,6 +56,94 @@ void DecodingHuffman(){
       cout<<"Файл не открывается.Исправьте ошибку!";
 	  exit(0);
     }
+	unsigned char count = 0;
+    unsigned int count_simvol = 0;
+    count = fgetc(input);
+    if (!feof(input))
+    {
+       count_simvol =(unsigned int) count;
+    }
+
+    unsigned char c = 0;
+
+    for (int i = 0; i < count_simvol; i++)
+    {
+        c = fgetc(input);
+        if (!feof(input))
+        {
+            fread(reinterpret_cast<char*>(&ABC[c]), sizeof(long int), 1, input);
+        }
+        else
+        {
+            cout<<"Распаковка файла не произошла.Ошибка!";
+		exit(0);
+        }
+    }
+
+    priority_queue<Node, vector<Node>, Node> htree;
+    for (int i = 0; i < 256; i++)
+    {
+        if (ABC[i] != 0)
+        {
+            string s(1, static_cast<char>(i));
+
+            Node newhtree(s, ABC[i]);
+            htree.push(newhtree);
+        }
+    }
+    c = 0;
+    Node *t = BuildTree(htree);
+
+    FILE* output = fopen("output.txt", "w+");
+    Node *nodes = t;
+    unsigned char simvol = 0;
+    while (!feof(input))
+    {  
+        c = fgetc(input);
+        if (!feof(input))
+        {
+            for (int i = 7; i > -1; i--)
+            {
+                if (((c >> i) & 1) == 1) 
+                {
+                    if (nodes->right == NULL)
+                    {
+                        simvol = nodes->key[0];
+                        if (ABC[simvol] > 0)
+                        {
+                            ABC[simvol]--;
+                            fputc(simvol, output);
+                            nodes = t->right;
+                        }
+                    }
+                    else
+                    {
+                        nodes = nodes->right;
+                    }
+                }
+                else if (((c >> i) & 1) == 0)
+                {
+                    if (nodes->left == NULL)
+                    {
+                        simvol = nodes->key[0];
+                        if (ABC[simvol] > 0)
+                        {
+                            fputc(simvol, output);
+                            nodes = t->left;
+                            ABC[simvol]--;
+                        }
+                    }
+                    else
+                    {
+                        nodes = nodes->left;
+                    }
+                }
+            }
+       }
+    }
+
+    fclose(input);
+    fclose(output);
 }
 
 int main(){
